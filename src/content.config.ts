e939faab70184c71e271e8dff71b73c3e7394f99
f3ve <free.eve@pm.me>
2025-03-31 17:07:44 -0800

    created authors

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,18 +1,28 @@
 import { glob } from 'astro/loaders';
-import { defineCollection, z } from 'astro:content';
+import { defineCollection, reference, z } from 'astro:content';
 
 const blog = defineCollection({
-  // Load Markdown and MDX files in the `src/content/blog/` directory.
   loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
-  // Type-check frontmatter using a schema
   schema: z.object({
     title: z.string(),
     description: z.string(),
-    // Transform string to Date object
     pubDate: z.coerce.date(),
     updatedDate: z.coerce.date().optional(),
     heroImage: z.string().optional(),
+    author: reference('authors'),
+    relatedPosts: z.array(reference('blog')).optional(),
+    tags: z.array(z.string()).optional(),
   }),
 });
 
-export const collections = { blog };
+const authors = defineCollection({
+  loader: glob({ pattern: '**/[^_]*.json', base: './src/content/authors' }),
+  schema: z.object({
+    name: z.string(),
+    username: z.string(),
+    bio: z.string(),
+    avatar: z.string().url(),
+  }),
+});
+
+export const collections = { blog, authors };
